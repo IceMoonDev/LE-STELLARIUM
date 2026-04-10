ea1cc51bbf3f54ebe6b8971c2ca196cb74250c4d
Amaury Compin <amaurycompin@gmail.com>
2026-04-10 15:00:31 +0200

    [Fix] Portal is now vertical and working with wall

--- LE-STELLARIUM/Assets/Scripts/PortalGun.cs
+++ LE-STELLARIUM/Assets/Scripts/PortalGun.cs
@@ -5,73 +5,64 @@ public class PortalGun : MonoBehaviour
 {
     Camera cam;
 
-    // NEW: We now have two separate slots for your prefabs
     public GameObject bluePortalPrefab;
     public GameObject orangePortalPrefab;
     public LayerMask layer;
-
     public Transform pivot;
-    public GameObject truc;
 
-    PlayerInputAction playerInputAction;
+    public float epaisseurMaxMur = 5f;
 
     void Start()
     {
         cam = Camera.main;
-        playerInputAction = new PlayerInputAction();
-        playerInputAction.Enable();
     }
 
     void Update()
     {
         if (Mouse.current == null) return;
 
-        Vector2 mousePos = playerInputAction.InGame.mousePos.ReadValue<Vector2>();
-        Vector2 screenPoint = cam.WorldToScreenPoint(transform.localPosition);
+        Vector2 mouseScreenPosition = Mouse.current.position.ReadValue();
+        Vector2 mouseWorldPosition = cam.ScreenToWorldPoint(mouseScreenPosition);
 
-        Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
-        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        Vector2 direction = mouseWorldPosition - (Vector2)pivot.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         pivot.rotation = Quaternion.Euler(0, 0, angle);
 
-        if (truc != null)
-        {
-            truc.transform.position = mousePos;
-        }
-
-        // LEFT CLICK = Blue Portal
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, mousePos, Mathf.Infinity, layer);
-            if (Physics2D.Raycast(transform.position, mousePos, Mathf.Infinity, layer))
-            {
-                Instantiate(bluePortalPrefab, hit.point, Quaternion.identity);
-                var width = hit.collider.gameObject.transform.localScale.x;
-                Instantiate(orangePortalPrefab, hit.point + Vector2.right * width, Quaternion.identity);
-            }
-            //ShootPortal(bluePortalPrefab, "blue portal");
+            ShootDoublePortal(bluePortalPrefab, "blue portal", orangePortalPrefab, "orange portal", direction);
         }
 
-        // RIGHT CLICK = Orange Portal
         if (Mouse.current.rightButton.wasPressedThisFrame)
         {
-            ShootPortal(orangePortalPrefab, "orange portal");
+            ShootDoublePortal(orangePortalPrefab, "orange portal", bluePortalPrefab, "blue portal", direction);
         }
     }
 
-    // Helper function to handle the shooting logic for both colors
-    void ShootPortal(GameObject prefabToShoot, string tagToLookFor)
+    void ShootDoublePortal(GameObject frontPrefab, string frontTag, GameObject backPrefab, string backTag, Vector2 direction)
     {
-        Vector3 mousePos = Mouse.current.position.ReadValue();
-        Vector2 cursorPos = cam.ScreenToWorldPoint(mousePos);
+        RaycastHit2D hitFront = Physics2D.Raycast(pivot.position, direction.normalized, Mathf.Infinity, layer);
 
-        // Find the specific color portal and destroy it
-        GameObject oldPortal = GameObject.FindGameObjectWithTag(tagToLookFor);
-        if (oldPortal != null)
+        if (hitFront.collider != null)
         {
-            Destroy(oldPortal);
-        }
+            GameObject oldFrontPortal = GameObject.FindGameObjectWithTag(frontTag);
+            if (oldFrontPortal != null) Destroy(oldFrontPortal);
 
-        // Spawn the new portal
-        Instantiate(prefabToShoot, new Vector3(cursorPos.x, cursorPos.y, 0), Quaternion.identity);
+            Quaternion frontRotation = Quaternion.FromToRotation(Vector2.right, hitFront.normal);
+            Instantiate(frontPrefab, hitFront.point, frontRotation);
+
+            Vector2 pointDerriereMur = hitFront.point - (hitFront.normal * epaisseurMaxMur);
+
+            RaycastHit2D hitBack = Physics2D.Raycast(pointDerriereMur, hitFront.normal, epaisseurMaxMur, layer);
+
+            if (hitBack.collider != null)
+            {
+                GameObject oldBackPortal = GameObject.FindGameObjectWithTag(backTag);
+                if (oldBackPortal != null) Destroy(oldBackPortal);
+
+                Quaternion backRotation = Quaternion.FromToRotation(Vector2.right, hitBack.normal);
+                Instantiate(backPrefab, hitBack.point, backRotation);
+            }
+        }
     }
 }
